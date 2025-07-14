dbdecb12c28006c17be124697dcd061dfb05136e
Khæi <boom.raeiy@gmail.com>
2025-07-14 08:11:38 +0800

    Prepping for paddle movement

--- Assets/Script/CharacterController.cs
+++ Assets/Script/CharacterController.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Player Movement
+    private Vector3 playerVelocity;
+    private Vector2 movementInput = Vector2.zero;
+    public float playerSpeed = 4.5f;
 
-    // Update is called once per frame
+    public Rigidbody2D rb;
+
+    void OnMove(InputAction.CallbackContext context) { movementInput = context.ReadValue<Vector2>(); }
+
+    /*
     void Update()
     {
-        
+        if (movementInput != Vector2.zero)
+        {
+            // Determine the angle of the movement input
+            float angle = Mathf.Atan2(movementInput.x, movementInput.y) * Mathf.Rad2Deg;
+
+            rb.AddForce(angle * Time.deltaTime * playerSpeed, ForceMode2D.Force);
+            transform.forward = moveDir;
+        }
+
+        playerVelocity.y += Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
+    */
 }
